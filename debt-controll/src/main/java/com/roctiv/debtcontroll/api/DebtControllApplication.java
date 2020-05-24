9214c4c03be0e677f5b2a3d322e8b6bd5d1d7ef7
joao547 <joaobezerra547@gmail.com>
2020-05-24 16:04:46 -0300

    enviando o projeto raiz da API

---
+++ debt-controll/src/main/java/com/roctiv/debtcontroll/api/DebtControllApplication.java
@@ -0,0 +1,13 @@
+package com.roctiv.debtcontroll.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtControllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebtControllApplication.class, args);
+	}
+
+}
---
+++ debt-controll/src/test/java/com/roctiv/debtcontroll/api/DebtControllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roctiv.debtcontroll.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtControllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
